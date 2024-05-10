2eaca93b971fec6cee5b573bdf837b18de740eb2
Luiz Monferrari <lmonferrrai@outlook.com>
2024-05-10 19:22:54 -0300

    commit inicial com api e modelo

---
+++ main.py
@@ -0,0 +1,24 @@
+import io
+import uvicorn
+from modelo import model_pipeline
+from PIL import Image
+from fastapi import FastAPI, UploadFile, File, Form
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"API": "Reconhecimento de texto em imagem."}
+
+
+@app.post("/api/v1/")
+async def api_v1(text: str = Form(...), image: UploadFile = File(...)):
+    image_contents = await image.read()
+    image = Image.open(io.BytesIO(image_contents))
+    predict = model_pipeline(text, image)
+    return {"predict": predict}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app)
